a95acc4545414342dc1b92c2130d16830d067aae
Ronald Kwan <rkwan@berkeley.edu>
2015-10-09 11:00:14 -0700

    add filtering to API

--- ucbgradedists/core/api/resources.py
+++ ucbgradedists/core/api/resources.py
@@ -1,16 +1,25 @@
 from tastypie import fields
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from core.models import Term, Subject, Course, Section, Grade
 
 class TermResource(ModelResource):
     class Meta:
         queryset = Term.objects.all()
         allowed_methods = ['get']
+        fitering = {
+            'season': ALL,
+            'year': ALL,
+            'id': ALL,
+        }
 
 class SubjectResource(ModelResource):
     class Meta:
         queryset = Subject.objects.all()
         allowed_methods = ['get']
+        filtering = {
+            'name': ALL,
+            'id': ALL,
+        }
 
 class CourseResource(ModelResource):
     subject = fields.ForeignKey(SubjectResource, 'subject')
@@ -18,6 +27,11 @@ class CourseResource(ModelResource):
     class Meta:
         queryset = Course.objects.all()
         allowed_methods = ['get']
+        filtering = {
+            'subject': ALL_WITH_RELATIONS,
+            'number': ALL,
+            'id': ALL,
+        }
 
 class SectionResource(ModelResource):
     course = fields.ForeignKey(CourseResource, 'course')
@@ -27,6 +41,12 @@ class SectionResource(ModelResource):
     class Meta:
         queryset = Section.objects.all()
         allowed_methods = ['get']
+        filtering = {
+            'course': ALL_WITH_RELATIONS,
+            'term': ALL_WITH_RELATIONS,
+            'ccn': ALL,
+            'id': ALL,
+        }
 
     def dehydrate_grade_average(self, bundle):
         return bundle.obj.grade_average
@@ -35,3 +55,6 @@ class GradeResource(ModelResource):
     class Meta:
         queryset = Grade.objects.all()
         allowed_methods = ['get']
+        filtering = {
+            'id': ALL,
+        }
