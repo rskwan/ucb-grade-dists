8309f7df6940f36cd453e8c76c5c87b26e060999
Sahil Chinoy <sahil.chinoy@gmail.com>
2015-11-16 17:54:26 -0800

    Added timeseries chart

--- ucbgradedists/static/js/grade_histogram.js
+++ ucbgradedists/static/js/grade_histogram.js
@@ -1,64 +1,3 @@
-/*
- * Convert arbitrary strings to valid css classes.
- * via: https://gist.github.com/mathewbyrne/1280286
- *
- * NOTE: This implementation must be consistent with the Python classify
- * function defined in base_filters.py.
- */
-var classify = function(str) {
-    return str.toLowerCase()
-        .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
-}
-
-/*
- * Convert key/value pairs to a style string.
- */
-var formatStyle = function(props) {
-    var s = '';
-
-    for (var key in props) {
-        s += key + ': ' + props[key].toString() + '; ';
-    }
-
-    return s;
-}
-
-/*
- * Create a SVG tansform for a given translation.
- */
-var makeTranslate = function(x, y) {
-    var transform = d3.transform();
-
-    transform.translate[0] = x;
-    transform.translate[1] = y;
-
-    return transform.toString();
-}
-
-/*
- * Parse a url parameter by name.
- * via: http://stackoverflow.com/a/901144
- */
-var getParameterByName = function(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-/*
- * Convert a url to a location object.
- */
-var urlToLocation = function(url) {
-    var a = document.createElement('a');
-    a.href = url;
-    return a;
-}
-
 // Global config
 var GRAPHIC_DEFAULT_WIDTH = 600;
 var MOBILE_THRESHOLD = 500;
@@ -72,7 +11,7 @@ var fmtComma = d3.format(',');
 /*
  * Format graphic data for processing by D3.
  */
-var formatData = function(data) {
+var formatHistogramData = function(data) {
     var graphicData = _.sortBy(data, function(d) {
       var value = 0;
       var letter = d.label[0];
@@ -108,12 +47,12 @@ var formatData = function(data) {
  * Render the graphic(s). Called by pym with the container width.
  */
 var renderHistogram = function(data, container) {
-    var graphicData = formatData(data);
+    var graphicData = formatHistogramData(data);
 
     // Set up resize event listener
     $( window ).resize(function() {
         renderColumnChart({
-            container: '#histogram',
+            container: container,
             data: graphicData
         });
     });
